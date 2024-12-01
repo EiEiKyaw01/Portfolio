ba861db1420dff9e3b548571137f41c36f9e1450
EiEiKyaw <11eikyw@gmail.com>
2024-12-01 21:20:36 +0630

    updating font and bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins: ["Poppins", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
