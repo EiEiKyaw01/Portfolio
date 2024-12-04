887b33693022d83c2f10fe89024058ce40754cad
EiEiKyaw <11eikyw@gmail.com>
2024-12-04 13:46:28 +0630

    add scroll reveal

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
 import './main.css';
+
+var effect = ScrollReveal({
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  origin: 'bottom',
+  viewFactor: 0.5,
+});
+
+effect.reveal('#hero-section', {
+  origin: 'top',
+  delay: 0
+});
+
+effect.reveal('#skills', {
+  origin: 'left',
+  viewFactor: 0.5,
+  delay: 500
+});
+
+effect.reveal('#project-section', {
+  origin: 'right',
+  viewFactor: 0.5,
+  delay: 500
+});
+
+effect.reveal('#contact-me', {
+  origin: 'bottom',
+  viewFactor: 0.5,
+  delay: 500
+});
