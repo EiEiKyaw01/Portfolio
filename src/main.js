10d73d7a56fbfbdc2646a62409e44e04bd800aaf
EiEiKyaw <11eikyw@gmail.com>
2024-12-05 20:47:43 +0630

    update project section

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import './main.css';
 var effect = ScrollReveal({
   distance: '60px',
   duration: 2500,
-  delay: 400,
-  origin: 'bottom',
-  viewFactor: 0.5,
 });
 
 effect.reveal('#hero-section', {
@@ -15,18 +12,19 @@ effect.reveal('#hero-section', {
 
 effect.reveal('#skills', {
   origin: 'left',
-  viewFactor: 0.5,
+  viewFactor: 0.3,
   delay: 500
 });
 
 effect.reveal('#project-section', {
   origin: 'right',
-  viewFactor: 0.5,
+  viewFactor: 0.3,
   delay: 500
 });
 
 effect.reveal('#contact-me', {
   origin: 'bottom',
-  viewFactor: 0.5,
+  viewFactor: 0.3,
   delay: 500
 });
+
