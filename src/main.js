bb56902019afa2bf5384debb404c7c78351a7eb7
EiEiKyaw <11eikyw@gmail.com>
2024-12-06 12:44:40 +0630

    fix small details and animation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './main.css';
 var effect = ScrollReveal({
   distance: '60px',
   duration: 2500,
+  origin: 'left'
 });
 
 effect.reveal('#hero-section', {
@@ -11,25 +12,21 @@ effect.reveal('#hero-section', {
 });
 
 effect.reveal("#aboutMe", {
-  origin: "left",
   viewFactor: 0.5,
   delay: 500,
 });
 
 effect.reveal('#skills', {
-  origin: 'right',
   viewFactor: 0.5,
   delay: 500
 });
 
 effect.reveal('#project-section', {
-  origin: 'left',
   viewFactor: 0.3,
   delay: 500
 });
 
 effect.reveal('#contact-me', {
-  origin: 'right',
   viewFactor: 0.2,
   delay: 500
 });
