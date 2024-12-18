4c51921eb43c82ef4401af81bb11190f54e63552
Akikyaw <11eikyw@gmail.com>
2024-12-18 15:31:38 +0630

    edit scrolling

--- src/main.js
+++ src/main.js
@@ -3,31 +3,27 @@ import './main.css';
 var effect = ScrollReveal({
   distance: '60px',
   duration: 2500,
-  origin: 'left'
+  origin: 'left',
+  delay: 100
 });
 
 effect.reveal('#hero-section', {
   origin: 'top',
-  delay: 0
 });
 
 effect.reveal("#aboutMe", {
-  viewFactor: 0.5,
-  delay: 500,
+  viewFactor: 0.2,
 });
 
 effect.reveal('#skills', {
-  viewFactor: 0.5,
-  delay: 500
+  viewFactor: 0.2,
 });
 
 effect.reveal('#project-section', {
-  viewFactor: 0.3,
-  delay: 500
+  viewFactor: 0.2,
 });
 
 effect.reveal('#contact-me', {
   viewFactor: 0.2,
-  delay: 500
 });
 
